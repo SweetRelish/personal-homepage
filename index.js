907931e80ae759115801556038d15a34c2d7970d
Qiang(Andrew) Yu <Andrew_yq@hotmail.com>
2015-12-06 18:25:44 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+(function() {
+	window.onload = function() {
+		var img = new Image();
+		img.src = "http://www.andrew-yq.com/flowerdancecover.jpg";
+		img.src = "http://www.andrew-yq.com/motacover.png";	
+	}
+})();
